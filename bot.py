10e36a03bbcfcea9604939110acc16228e723ef2
piercingecho <107562412+piercingecho@users.noreply.github.com>
2023-07-17 02:05:25 -0400

    Update bot.py to Utilize TwitchListen.py
    Mobilizes Discord bot to use twitch-discord integration in TwitchListen.py.
    Not a breaking change.

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@ import os
 from dotenv import load_dotenv
 import json
 from cogs.publish import Publish
+from cogs.twitchlisten import TwitchListen
+import cogs.twitchlisten
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-
 def run_discord_bot():
     # Initializing the intents of the bot
     intent = discord.Intents.default()
@@ -20,8 +21,12 @@ def run_discord_bot():
         print(f'{bot.user} is now running')
         bot.tree.clear_commands(guild=None)
         await bot.add_cog(Publish(bot))
+        await bot.add_cog(TwitchListen(bot))
         await bot.tree.sync()
         print('All commands set up!')
     
 
     bot.run(DISCORD_TOKEN)
+
+if __name__ == '__main__':
+    run_discord_bot()
