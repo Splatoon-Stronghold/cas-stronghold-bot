a5b5c598abd735daa7af73880e3b34d441673f92
Fisk <flaviusmus312@gmail.com>
2024-08-07 14:33:00 +0200

    Cleanup get_file_path() and start_time_to_json()
    rename start_time_to_json() -> save_start_time()

--- utils/start_time.py
+++ utils/start_time.py
@@ -4,23 +4,25 @@ import time
 import datetime
 import json
 
-FILENAME = "time.json"
-
 def get_file_path() -> str:
+    '''get_file_path
+    Returns
+    -------
+    data : str
+        The file path to time.json.
+    '''
+
     base_path = (Path(os.path.abspath(__file__)) /'..' /'..').resolve()
-    return base_path / FILENAME
+    return base_path / 'time.json'
 
-async def start_time_to_json():
-    start = time.time()
-
+async def save_start_time():
     dict = {
-        "start_time": start
+        "start_time": time.time()
     }
 
     json_object = json.dumps(dict, indent = 4)
 
-    with open(get_file_path(), "w") as outfile:
-        outfile.write(json_object)
+    with open(get_file_path(), "w") as json_file:
+        json_file.write(json_object)
         print("Done")
 
-    return start
